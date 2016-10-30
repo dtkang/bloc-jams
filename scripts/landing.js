b40ecc2419343cc15a53fabc66e6996a6bbbb82c
Dan Kang <dtkang@gmail.com>
2016-10-30 03:15:05 +0000

    Checkpoint 23 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,5 +1,6 @@
-var animatePoints = function() {
-    var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
 
     var revealPoint = function(arr) {
         for (var i=0; i<arr.length; i++) {
@@ -12,4 +13,15 @@ var animatePoints = function() {
     revealPoint(points);
 };
 
-animatePoints();
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function(event){
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
